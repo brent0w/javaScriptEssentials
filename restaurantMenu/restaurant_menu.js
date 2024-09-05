feafd775ba3c6b61b6788be3f49ac28f305bc98f
“Brent” <brentdwaller@gmail.com>
2024-09-05 18:30:21 -0400

    Added restaurant menu

---
+++ restaurantMenu/restaurant_menu.js
@@ -0,0 +1,20 @@
+const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
+const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+
+// Use map to get breakfast item list
+const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+        document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
+// Iterate through main course array wirh forEach
+let mainCourseItem = '';
+mainCourseMenu.forEach((item, index) => {
+    mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;
+});
+document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
+
+// Iterate through dessert array with for loop
+let dessertItem = '';
+for (let i = 0; i < dessertMenu.length; i++) {
+    dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;}
+document.getElementById('dessertMenuItems').innerHTML = dessertItem;
